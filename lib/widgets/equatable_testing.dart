bfd6a982271c13ba2656e6631fade7567dd25dcc
Asaduzzaman_mac <samrat.asad@gmail.com>
2025-02-04 18:59:07 +0600

    equatable package

--- lib/widgets/equatable_testing.dart
+++ lib/widgets/equatable_testing.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:equatable/equatable.dart';
 
 class EquatableTesting extends StatefulWidget {
   const EquatableTesting({super.key});
@@ -13,22 +13,23 @@ class _EquatableTestingState extends State<EquatableTesting> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: () {
+        Person person1 = Person(name: "samrat", age: 30);
+        Person person2 = Person(name: "samrat", age: 30);
 
-          Person person1 = Person(name: "samrat", age: 30);
-          Person person2 = Person(name: "samrat", age: 30);
-
-          print(person1 == person2);
-          print(person1 == person1);
-          print(person2 == person2);
-
+        print(person1 == person2);
+        print(person1 == person1);
+        print(person2 == person2);
       }),
     );
   }
 }
 
-class Person {
+class Person extends Equatable {
   final String name;
   final int age;
 
   const Person({required this.name, required this.age});
+
+  @override
+  List<Object?> get props => [name, age];
 }
